67da6c907927a0479421b4d56fd170a695f1380b
Bullets-jyr <jyr@bullets.co.kr>
2023-07-30 02:27:00 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/bullets/borutoapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package kr.co.bullets.borutoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.bullets.borutoapp.ui.theme.BorutoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BorutoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BorutoAppTheme {
+        Greeting("Android")
+    }
+}
