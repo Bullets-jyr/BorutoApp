a5d573d1321a35e522bc838cbffa9504511a67fa
Bullets-jyr <jyr@bullets.co.kr>
2023-08-16 18:32:47 +0900

    29. Setup Navigation

--- app/src/main/java/kr/co/bullets/borutoapp/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/borutoapp/MainActivity.kt
@@ -3,14 +3,20 @@ package kr.co.bullets.borutoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import kr.co.bullets.borutoapp.navigation.SetupNavGraph
 import kr.co.bullets.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/kr/co/bullets/borutoapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package kr.co.bullets.borutoapp.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
