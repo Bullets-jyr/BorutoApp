94696bffe77d1e30a52ea7c99259bea12fa01b1b
Bullets-jyr <jyr@bullets.co.kr>
2023-08-17 18:19:00 +0900

    35. Design Splash Screen

---
+++ app/src/main/java/kr/co/bullets/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,66 @@
+package kr.co.bullets.borutoapp.presentation.screens.splash
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import kr.co.bullets.borutoapp.R
+import kr.co.bullets.borutoapp.ui.theme.Purple500
+import kr.co.bullets.borutoapp.ui.theme.Purple700
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    Splash()
+}
+
+@Composable
+fun Splash() {
+    if (isSystemInDarkTheme()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SplashScreenPreview() {
+    Splash()
+}
+
+@Composable
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+fun SplashScreenDarkPreview() {
+    Splash()
+}
