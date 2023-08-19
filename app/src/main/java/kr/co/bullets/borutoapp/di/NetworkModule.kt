c283064cb998937b176670d941b6255ca05477c2
Bullets-jyr <jyr@bullets.co.kr>
2023-08-19 17:15:51 +0900

    45. Create NetworkModule object

---
+++ app/src/main/java/kr/co/bullets/borutoapp/di/NetworkModule.kt
@@ -0,0 +1,59 @@
+package kr.co.bullets.borutoapp.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import kr.co.bullets.borutoapp.data.local.BorutoDatabase
+import kr.co.bullets.borutoapp.data.remote.BorutoApi
+import kr.co.bullets.borutoapp.util.Constants.BASE_URL
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
+        return retrofit.create(BorutoApi::class.java)
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideRemoteDataSource(
+//        borutoApi: BorutoApi,
+//        borutoDatabase: BorutoDatabase
+//    ): RemoteDataSource {
+//        return RemoteDataSourceImpl(
+//            borutoApi = borutoApi,
+//            borutoDatabase = borutoDatabase
+//        )
+//    }
+}
