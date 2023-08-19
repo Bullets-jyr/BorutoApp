ad930d5c1e444e2bc2f478193a4a418da2cb1460
Bullets-jyr <jyr@bullets.co.kr>
2023-08-19 16:32:07 +0900

    43. Home Screen - Design Top Bar

--- app/src/main/java/kr/co/bullets/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/kr/co/bullets/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,15 @@
 package kr.co.bullets.borutoapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {
 
+    }
 }
---
+++ app/src/main/java/kr/co/bullets/borutoapp/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,47 @@
+package kr.co.bullets.borutoapp.presentation.screens.home
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.bullets.borutoapp.R
+import kr.co.bullets.borutoapp.ui.theme.topAppBarBackgroundColor
+import kr.co.bullets.borutoapp.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Explore",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
+
+//@Composable
+//@Preview(uiMode = UI_MODE_NIGHT_YES)
+//fun HomeTopBarDarkPreview() {
+//    HomeTopBar {}
+//}
